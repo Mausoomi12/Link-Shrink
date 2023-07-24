6027ba5ce26147d8d7d2918c921d888c7a109533
Mausoomi Roy <mausoomiroy68613@gmail.com>
2023-07-25 01:12:15 +0530

    initial commit

---
+++ models/url.model.js
@@ -0,0 +1,16 @@
+const mongoose  =require('mongoose')
+const Schema = mongoose.Schema
+
+const ShortUrlSchema= new Schema({
+    url: {
+        type:String,
+        required:true,
+    },
+    shortId:{
+        type:String,
+        requires:true,
+    }
+})
+
+const ShortUrl= mongoose.model('shortUrl',ShortUrlSchema)
+module.exports= ShortUrl;
